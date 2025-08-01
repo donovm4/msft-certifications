65e0e9af33984ce45f9560cf1d54d22802959217
Donovan McCoy <slycemccoy@gmail.com>
2025-08-01 11:48:59 -0400

    encrypted_storage demo infra

---
+++ fundamentals/sc-900/encrypted_storage/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 1.12, < 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0, < 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-sc900-storage-encryption"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = module.naming.storage_account.name_unique
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                       = module.naming.key_vault.name_unique
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  sku_name = "standard"
+}
